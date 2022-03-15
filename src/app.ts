b93020dc5cc0f04aa51688304262a05afc29ad79
Rafael Ortiz <rafaeldavid184@gmail.com>
2022-03-15 11:43:53 -0400

    fix update routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 import express from "express";
+import cors from 'cors';
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
 import { ratingWidgetConnectionHandler } from "./socket/connectionHandlers/ratingWidgetConnectionHandler";
+import { activeSessionsRouter } from './routes/activeSessions';
 
 dotenv.config({ path: __dirname + '/../.env' });
 
@@ -16,10 +18,14 @@ const io = new Server(httpServer, {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// routes
+app.use(activeSessionsRouter);
 
 io.of('/ratingWidget').on('connection', (socket: Socket) => {
     ratingWidgetConnectionHandler(socket, io);
