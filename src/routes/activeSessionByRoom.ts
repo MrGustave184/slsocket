5e6eb9cd702849b31101447516674b8c0da8202f
Rafael Ortiz <rafaeldavid184@gmail.com>
2022-04-07 10:23:55 -0400

    add session by room endpoint

---
+++ src/routes/activeSessionByRoom.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import { query, validationResult } from "express-validator";
+import { ActiveSessionModel } from "../models/activeSession";
+
+const router = express.Router();
+
+router.get('/api/activeSessionByRoom',
+    query('clientId').not().isEmpty().withMessage('please provide a valid client ID'),
+    query('projectId').not().isEmpty().withMessage('please provide a valid project ID'),
+    query('room').not().isEmpty().withMessage('please provide a valid room'),
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.array())
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { clientId, projectId, room } = req.query;
+        const activeSession = await ActiveSessionModel.findOne({clientId: clientId, projectId: projectId, room: room });
+
+        res.send({ activeSession });
+    }
+)
+
+export { router as activeSessionsRouter }
